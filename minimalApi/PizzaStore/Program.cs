a08b7656724548c19058ec57debb25b7d3fd7d37
donnyhyon <dhyon333@hotmail.co.uk>
2023-11-24 22:50:40 +0000

    added swagger integration

--- minimalApi/PizzaStore/Program.cs
+++ minimalApi/PizzaStore/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+// Can add Services to builder. e.g `builder.Services.AddCors(options => {});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+   {
+       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+   });
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+   {
+     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+   });
+
+// App instance sets-up routing.
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
